820c63cf2dec2045e4fd44e245fc0d3a8c68ff74
hassanmubiru <hassanmubiru7@gmail.com>
2026-03-20 17:51:02 +0300

    Changes to be committed: 	modified:   android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java 	modified:   ios/Runner/GeneratedPluginRegistrant.m

--- ios/Runner/GeneratedPluginRegistrant.m
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -48,124 +48,16 @@
 @import flutter_webrtc;
 #endif
 
-#if __has_include(<google_mlkit_barcode_scanning/GoogleMlKitBarcodeScanningPlugin.h>)
-#import <google_mlkit_barcode_scanning/GoogleMlKitBarcodeScanningPlugin.h>
-#else
-@import google_mlkit_barcode_scanning;
-#endif
-
 #if __has_include(<google_mlkit_commons/GoogleMlKitCommonsPlugin.h>)
 #import <google_mlkit_commons/GoogleMlKitCommonsPlugin.h>
 #else
 @import google_mlkit_commons;
 #endif
 
-#if __has_include(<google_mlkit_digital_ink_recognition/GoogleMlKitDigitalInkRecognitionPlugin.h>)
-#import <google_mlkit_digital_ink_recognition/GoogleMlKitDigitalInkRecognitionPlugin.h>
-#else
-@import google_mlkit_digital_ink_recognition;
-#endif
-
-#if __has_include(<google_mlkit_entity_extraction/GoogleMlKitEntityExtractionPlugin.h>)
-#import <google_mlkit_entity_extraction/GoogleMlKitEntityExtractionPlugin.h>
-#else
-@import google_mlkit_entity_extraction;
-#endif
-
-#if __has_include(<google_mlkit_face_detection/GoogleMlKitFaceDetectionPlugin.h>)
-#import <google_mlkit_face_detection/GoogleMlKitFaceDetectionPlugin.h>
-#else
-@import google_mlkit_face_detection;
-#endif
-
-#if __has_include(<google_mlkit_face_mesh_detection/GoogleMlKitFaceMeshDetectionPlugin.h>)
-#import <google_mlkit_face_mesh_detection/GoogleMlKitFaceMeshDetectionPlugin.h>
-#else
-@import google_mlkit_face_mesh_detection;
-#endif
-
-#if __has_include(<google_mlkit_genai_image_description/GoogleMlKitGenaiImageDescriptionPlugin.h>)
-#import <google_mlkit_genai_image_description/GoogleMlKitGenaiImageDescriptionPlugin.h>
-#else
-@import google_mlkit_genai_image_description;
-#endif
-
-#if __has_include(<google_mlkit_genai_prompt/GoogleMlKitGenaiPromptPlugin.h>)
-#import <google_mlkit_genai_prompt/GoogleMlKitGenaiPromptPlugin.h>
-#else
-@import google_mlkit_genai_prompt;
-#endif
-
-#if __has_include(<google_mlkit_genai_proofreading/GoogleMlKitGenaiProofreadingPlugin.h>)
-#import <google_mlkit_genai_proofreading/GoogleMlKitGenaiProofreadingPlugin.h>
-#else
-@import google_mlkit_genai_proofreading;
-#endif
-
-#if __has_include(<google_mlkit_genai_rewriting/GoogleMlKitGenaiRewritingPlugin.h>)
-#import <google_mlkit_genai_rewriting/GoogleMlKitGenaiRewritingPlugin.h>
-#else
-@import google_mlkit_genai_rewriting;
-#endif
-
-#if __has_include(<google_mlkit_genai_speech_recognition/GoogleMlKitGenaiSpeechRecognitionPlugin.h>)
-#import <google_mlkit_genai_speech_recognition/GoogleMlKitGenaiSpeechRecognitionPlugin.h>
-#else
-@import google_mlkit_genai_speech_recognition;
-#endif
-
-#if __has_include(<google_mlkit_genai_summarization/GoogleMlKitGenaiSummarizationPlugin.h>)
-#import <google_mlkit_genai_summarization/GoogleMlKitGenaiSummarizationPlugin.h>
-#else
-@import google_mlkit_genai_summarization;
-#endif
-
-#if __has_include(<google_mlkit_image_labeling/GoogleMlKitImageLabelingPlugin.h>)
-#import <google_mlkit_image_labeling/GoogleMlKitImageLabelingPlugin.h>
-#else
-@import google_mlkit_image_labeling;
-#endif
-
-#if __has_include(<google_mlkit_language_id/GoogleMlKitLanguageIdPlugin.h>)
-#import <google_mlkit_language_id/GoogleMlKitLanguageIdPlugin.h>
-#else
-@import google_mlkit_language_id;
-#endif
-
-#if __has_include(<google_mlkit_object_detection/GoogleMlKitObjectDetectionPlugin.h>)
-#import <google_mlkit_object_detection/GoogleMlKitObjectDetectionPlugin.h>
-#else
-@import google_mlkit_object_detection;
-#endif
-
-#if __has_include(<google_mlkit_pose_detection/GoogleMlKitPoseDetectionPlugin.h>)
-#import <google_mlkit_pose_detection/GoogleMlKitPoseDetectionPlugin.h>
-#else
-@import google_mlkit_pose_detection;
-#endif
-
-#if __has_include(<google_mlkit_selfie_segmentation/GoogleMlKitSelfieSegmentationPlugin.h>)
-#import <google_mlkit_selfie_segmentation/GoogleMlKitSelfieSegmentationPlugin.h>
-#else
-@import google_mlkit_selfie_segmentation;
-#endif
-
-#if __has_include(<google_mlkit_smart_reply/GoogleMlKitSmartReplyPlugin.h>)
-#import <google_mlkit_smart_reply/GoogleMlKitSmartReplyPlugin.h>
-#else
-@import google_mlkit_smart_reply;
-#endif
-
 #if __has_include(<google_mlkit_text_recognition/GoogleMlKitTextRecognitionPlugin.h>)
 #import <google_mlkit_text_recognition/GoogleMlKitTextRecognitionPlugin.h>
 #else
 @import google_mlkit_text_recognition;
-#endif
-
-#if __has_include(<google_mlkit_translation/GoogleMlKitTranslationPlugin.h>)
-#import <google_mlkit_translation/GoogleMlKitTranslationPlugin.h>
-#else
-@import google_mlkit_translation;
 #endif
 
 #if __has_include(<image_picker_ios/FLTImagePickerPlugin.h>)
@@ -256,26 +148,8 @@
   [FPPDeviceInfoPlusPlugin registerWithRegistrar:[registry registrarForPlugin:@"FPPDeviceInfoPlusPlugin"]];
   [FlutterSecureStorageDarwinPlugin registerWithRegistrar:[registry registrarForPlugin:@"FlutterSecureStorageDarwinPlugin"]];
   [FlutterWebRTCPlugin registerWithRegistrar:[registry registrarForPlugin:@"FlutterWebRTCPlugin"]];
-  [GoogleMlKitBarcodeScanningPlugin registerWithRegistrar:[registry registrarForPlugin:@"GoogleMlKitBarcodeScanningPlugin"]];
   [GoogleMlKitCommonsPlugin registerWithRegistrar:[registry registrarForPlugin:@"GoogleMlKitCommonsPlugin"]];
-  [GoogleMlKitDigitalInkRecognitionPlugin registerWithRegistrar:[registry registrarForPlugin:@"GoogleMlKitDigitalInkRecognitionPlugin"]];
-  [GoogleMlKitEntityExtractionPlugin registerWithRegistrar:[registry registrarForPlugin:@"GoogleMlKitEntityExtractionPlugin"]];
-  [GoogleMlKitFaceDetectionPlugin registerWithRegistrar:[registry registrarForPlugin:@"GoogleMlKitFaceDetectionPlugin"]];
-  [GoogleMlKitFaceMeshDetectionPlugin registerWithRegistrar:[registry registrarForPlugin:@"GoogleMlKitFaceMeshDetectionPlugin"]];
-  [GoogleMlKitGenaiImageDescriptionPlugin registerWithRegistrar:[registry registrarForPlugin:@"GoogleMlKitGenaiImageDescriptionPlugin"]];
-  [GoogleMlKitGenaiPromptPlugin registerWithRegistrar:[registry registrarForPlugin:@"GoogleMlKitGenaiPromptPlugin"]];
-  [GoogleMlKitGenaiProofreadingPlugin registerWithRegistrar:[registry registrarForPlugin:@"GoogleMlKitGenaiProofreadingPlugin"]];
-  [GoogleMlKitGenaiRewritingPlugin registerWithRegistrar:[registry registrarForPlugin:@"GoogleMlKitGenaiRewritingPlugin"]];
-  [GoogleMlKitGenaiSpeechRecognitionPlugin registerWithRegistrar:[registry registrarForPlugin:@"GoogleMlKitGenaiSpeechRecognitionPlugin"]];
-  [GoogleMlKitGenaiSummarizationPlugin registerWithRegistrar:[registry registrarForPlugin:@"GoogleMlKitGenaiSummarizationPlugin"]];
-  [GoogleMlKitImageLabelingPlugin registerWithRegistrar:[registry registrarForPlugin:@"GoogleMlKitImageLabelingPlugin"]];
-  [GoogleMlKitLanguageIdPlugin registerWithRegistrar:[registry registrarForPlugin:@"GoogleMlKitLanguageIdPlugin"]];
-  [GoogleMlKitObjectDetectionPlugin registerWithRegistrar:[registry registrarForPlugin:@"GoogleMlKitObjectDetectionPlugin"]];
-  [GoogleMlKitPoseDetectionPlugin registerWithRegistrar:[registry registrarForPlugin:@"GoogleMlKitPoseDetectionPlugin"]];
-  [GoogleMlKitSelfieSegmentationPlugin registerWithRegistrar:[registry registrarForPlugin:@"GoogleMlKitSelfieSegmentationPlugin"]];
-  [GoogleMlKitSmartReplyPlugin registerWithRegistrar:[registry registrarForPlugin:@"GoogleMlKitSmartReplyPlugin"]];
   [GoogleMlKitTextRecognitionPlugin registerWithRegistrar:[registry registrarForPlugin:@"GoogleMlKitTextRecognitionPlugin"]];
-  [GoogleMlKitTranslationPlugin registerWithRegistrar:[registry registrarForPlugin:@"GoogleMlKitTranslationPlugin"]];
   [FLTImagePickerPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTImagePickerPlugin"]];
   [JustAudioPlugin registerWithRegistrar:[registry registrarForPlugin:@"JustAudioPlugin"]];
   [MobileScannerPlugin registerWithRegistrar:[registry registrarForPlugin:@"MobileScannerPlugin"]];
